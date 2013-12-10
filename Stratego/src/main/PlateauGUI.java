28ef412b123de55e273beed37f94c8b265a9ec98
CLEMENT-LP <lphclement@hotmail.com>
2013-12-10 15:56:18 +0100

    Pions sur le tableau, interaction Mouse

--- Stratego/src/main/PlateauGUI.java
+++ Stratego/src/main/PlateauGUI.java
@@ -6,6 +6,7 @@ import javax.swing.border.LineBorder;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 /**
  * Interface graphique du plateau, utilise classe Plateau pour définir le visuel
  * +comportement des différents éléments
@@ -19,10 +20,119 @@ public class PlateauGUI extends JFrame
 	private Plateau plateau;
 	//!\\ ajouter tableaux rouge et bleu de départ
 	private JLabel result;
-	private caseButton [][] Cb;
-
+	private caseButton [][] cb;
+	private caseButton [][] cb1;
+	private caseButton [][] cb2;
+	private Unit pionCurrent;
+	private ArrayList<Unit> listePionsWhite=new ArrayList<Unit>();
+	private ArrayList<Unit> listePionsBlack=new ArrayList<Unit>();
+	
+	public void createUnit(){
+	Unit bmarechal=new Unit("marshal",10,"images/black/marshal.jpg");
+	Unit bgeneral=new Unit("general",9,"images/black/general.jpg");
+	Unit bcolonel=new Unit("Colonel",8,"images/black/colonel.jpg");
+	Unit bmajor=new Unit("major",7,"images/black/major.jpg");
+	Unit bcapitaine=new Unit("captain",6,"images/black/captain.jpg");
+	Unit blieutenant=new Unit("lieutnant",5,"images/black/leutnant.jpg");
+	Unit bsergent=new Unit("sergent",4,"images/black/sergent.jpg");
+	Unit beclaireur=new Unit("scout",2,"images/black/scout.jpg");
+	Unit bdemineur=new Unit("deminor",3,"images/black/deminor.jpg");
+	Unit bespion=new Unit("spy",1,"images/black/spy.jpg");
+	Unit bbombe=new Unit("bomb",11,"images/black/bomb.jpg");
+	Unit bdrapeau=new Unit("flag",0,"images/black/flag.jpg");
+	
+	Unit wmarechal=new Unit("marshal",10,"images/white/marshal.jpg");
+	Unit wgeneral=new Unit("general",9,"images/white/general.jpg");
+	Unit wcolonel=new Unit("Colonel",8,"images/white/colonel.jpg");
+	Unit wmajor=new Unit("major",7,"images/white/major.jpg");
+	Unit wcapitaine=new Unit("captain",6,"images/white/captain.jpg");
+	Unit wlieutenant=new Unit("lieutnant",5,"images/white/leutnant.jpg");
+	Unit wsergent=new Unit("sergent",4,"images/white/sergent.jpg");
+	Unit weclaireur=new Unit("scout",2,"images/white/scout.jpg");
+	Unit wdemineur=new Unit("deminor",3,"images/white/deminor.jpg");
+	Unit wespion=new Unit("spy",1,"images/white/spy.jpg");
+	Unit wbombe=new Unit("bomb",11,"images/white/bomb.jpg");
+	Unit wdrapeau=new Unit("flag",0,"images/white/flag.jpg");
+	
+	listePionsBlack.add(bmarechal);
+	listePionsBlack.add(bgeneral);
+	
+	for(int i=0;i<2;i++)
+	{
+		listePionsBlack.add(bcolonel);
+	}
+	for(int i=0;i<3;i++)
+	{
+		listePionsBlack.add(bmajor);
+	}
+	for(int i=0;i<4;i++)
+	{
+		listePionsBlack.add(bcapitaine);
+	}
+	for(int i=0;i<4;i++)
+	{
+		listePionsBlack.add(blieutenant);
+	}
+	for(int i=0;i<4;i++)
+	{
+		listePionsBlack.add(bsergent);
+	}
+	for(int i=0;i<5;i++)
+	{
+		listePionsBlack.add(bdemineur);
+	}
+	for(int i=0;i<8;i++)
+	{
+		listePionsBlack.add(beclaireur);
+	}
+	for(int i=0;i<6;i++)
+	{
+		listePionsBlack.add(bbombe);
+	}
+	listePionsBlack.add(bespion);
+	listePionsBlack.add(bdrapeau);
+	
+	listePionsWhite.add(wmarechal);
+	listePionsWhite.add(wgeneral);
+	
+	for(int i=0;i<2;i++)
+	{
+		listePionsWhite.add(wcolonel);
+	}
+	for(int i=0;i<3;i++)
+	{
+		listePionsWhite.add(wmajor);
+	}
+	for(int i=0;i<4;i++)
+	{
+		listePionsWhite.add(wcapitaine);
+	}
+	for(int i=0;i<4;i++)
+	{
+		listePionsWhite.add(wlieutenant);
+	}
+	for(int i=0;i<4;i++)
+	{
+		listePionsWhite.add(wsergent);
+	}
+	for(int i=0;i<5;i++)
+	{
+		listePionsWhite.add(wdemineur);
+	}
+	for(int i=0;i<8;i++)
+	{
+		listePionsWhite.add(weclaireur);
+	}
+	for(int i=0;i<6;i++)
+	{
+		listePionsWhite.add(wbombe);
+	}
+	listePionsWhite.add(wespion);
+	listePionsWhite.add(wdrapeau);
+	}
 	public PlateauGUI (Plateau plateau)
 	{
+		createUnit();//Liste des pions
 		this.setTitle("Stratego");//titre fenêtre
 		this.setLocation(200,200);//positionnement lors du lancement depuis bord supérieur gauche
 		this.plateau = plateau;//voir classe Plateau
@@ -38,7 +148,7 @@ public class PlateauGUI extends JFrame
 		JButton launch = new JButton("START");
 		launch.addActionListener (new LaunchButtonListener()); // bouton "Launch" avec écouteur d'action
 		//CONTENU des boutons des Grid (exemple basique à remplacer par les objets UNIT)
-		caseButton [][] cb = new caseButton [size][size]; 
+		cb = new caseButton [size][size]; 
 		for(int i=0; i < size; i++)
 		{
 			for(int j=0; j < size; j++)
@@ -49,35 +159,43 @@ public class PlateauGUI extends JFrame
 				cb [i][j].setBackground (Color.WHITE);
 				cb [i][j].setPreferredSize (new Dimension (50, 50));             // on cree les boutons et on les met tous blancs
 				jp.add (cb[i][j]);
-				cb [i][j].addActionListener (new ColorButtonListener());
+				cb [i][j].addActionListener(new ColorButtonListener());
 			}
 		}
 
-		caseButton [][] cb1 = new caseButton [5][8]; 
+		cb1 = new caseButton [5][8]; 
+		int compteur=0;
 		for(int i=0; i < 5; i++)
 		{
 			for(int j=0; j < 8; j++)
 			{	
-				Unit pion=new Unit("Colonel",8,"images/black/colonel.jpg");
+				Unit pion=listePionsWhite.get(compteur);
 				cb1 [i][j] = new caseButton(i,j,pion);
 				cb1 [i][j].setIcon(new ImageIcon(getClass().getClassLoader().getResource(pion.getImagePath())));
 				cb1 [i][j].setBackground (Color.RED);
 				cb1 [i][j].setPreferredSize (new Dimension (50, 50));             // on cree les boutons et on les met tous blancs
 				jpP1.add (cb1[i][j]);
-				cb1 [i][j].addActionListener (new ColorButtonListener());
+				//cb1 [i][j].addActionListener (new ColorButtonListener());
+				cb1[i][j].addActionListener(new MoveUnitListener());
+				compteur++;
+				//Unit temp=cb1[i][j].getPion();
 			}
 		}
-
-		caseButton [][] cb2 = new caseButton [5][8]; 
+		compteur=0;
+		cb2 = new caseButton [5][8]; 
 		for(int i=0; i < 5; i++)
 		{
 			for(int j=0; j < 8; j++)
 			{
-				cb2 [i][j] = new caseButton(i,j, null);
+				Unit pion=listePionsBlack.get(compteur);
+				cb2 [i][j] = new caseButton(i,j,pion);
+				cb2 [i][j].setIcon(new ImageIcon(getClass().getClassLoader().getResource(pion.getImagePath())));
 				cb2 [i][j].setBackground (Color.BLUE);
 				cb2 [i][j].setPreferredSize (new Dimension (50, 50));             // on cree les boutons et on les met tous blancs
 				jpP2.add (cb2[i][j]);
-				cb2 [i][j].addActionListener (new ColorButtonListener());
+				//cb2 [i][j].addActionListener (new ColorButtonListener());
+				cb2[i][j].addActionListener(new MoveUnitListener());
+				compteur++;
 			}
 		}
 
@@ -98,10 +216,28 @@ public class PlateauGUI extends JFrame
 		this.pack();
 		this.setVisible(true); // on rend visible la fenetre
 	}
-
+	
+	public caseButton[][] getCb() {
+		return cb;
+	}
+	public void setCb(caseButton[][] cb) {
+		this.cb = cb;
+	}
+	public caseButton[][] getCb1() {
+		return cb1;
+	}
+	public void setCb1(caseButton[][] cb1) {
+		this.cb1 = cb1;
+	}
+	public caseButton[][] getCb2() {
+		return cb2;
+	}
+	public void setCb2(caseButton[][] cb2) {
+		this.cb2 = cb2;
+	}
 
 	//!\\ Ajouter images/cases
-	private class caseButton extends JButton
+	private class caseButton extends JButton implements MouseListener
 	{
 		private int i; //ligne du bouton
 		private int j; //colonne du bouton
@@ -124,8 +260,58 @@ public class PlateauGUI extends JFrame
 		{
 			return j;
 		}
+		public Unit getPion()
+		{
+			return pion;
+		}
+		public String toString(){
+			return pion.getDescription();
+			
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
 	}
 
+	
+	private class MoveUnitListener implements ActionListener
+	{
+		public void actionPerformed (ActionEvent e)
+		{
+			//pionCurrent=cb[1][1].getPion;
+			caseButton pionNew= ((caseButton)e.getSource());
+			System.out.println(pionNew);
+			//Unit pionTemp=cb1[i][j].getPion();
+		}   
+	}
+	
 	private class LaunchButtonListener implements ActionListener
 	{
 		public void actionPerformed (ActionEvent e)
